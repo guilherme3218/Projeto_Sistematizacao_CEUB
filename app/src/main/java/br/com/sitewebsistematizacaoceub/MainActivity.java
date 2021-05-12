bac3b3f48e3985d3653793faa7277c725d5eccc5
Guilherme Porto <guilhermeportodossb@gmail.com>
2021-05-12 11:53:10 -0300

    Java

--- app/src/main/java/br/com/sitewebsistematizacaoceub/MainActivity.java
+++ app/src/main/java/br/com/sitewebsistematizacaoceub/MainActivity.java
@@ -3,7 +3,10 @@ package br.com.sitewebsistematizacaoceub;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,5 +18,11 @@ public class MainActivity extends AppCompatActivity {
 
         WebView webView = findViewById(R.id.webview);
         webView.loadUrl("https://www.ea.uniceub.br/");
+        webView.getSettings().setJavaScriptEnabled(true);
+
+        final ProgressBar progressBar = findViewById(R.id.progressBar);
+        progressBar.setVisibility(View.INVISIBLE);
     }
 }
+
+
